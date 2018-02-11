6013fc130d208760955b855bf969df1be8533a35
davelively14 <dave.lively@salesloft.com>
2018-02-11 08:45:35 -0500

    Init commit

---
+++ lib/data_backup.ex
@@ -0,0 +1,18 @@
+defmodule DataBackup do
+  @moduledoc """
+  Documentation for DataBackup.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> DataBackup.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/data_backup_test.exs
@@ -0,0 +1,8 @@
+defmodule DataBackupTest do
+  use ExUnit.Case
+  doctest DataBackup
+
+  test "greets the world" do
+    assert DataBackup.hello() == :world
+  end
+end
